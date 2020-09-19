5aec8faa2083c823b5b603a61c55d3642803ccd3
Uma Krishnan <u.krishnan@gmail.com>
2020-09-18 23:07:46 -0700

    remove .docz

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/4a1451c9ae49660f4890a9bdd05933af.js
+++
@@ -1,8 +0,0 @@
-import AvatarGroup from "../../../../../src/components/avatar-group/AvatarGroup";
-import { Playground } from 'docz';
-import * as React from 'react';
-export default {
-  AvatarGroup,
-  Playground,
-  React
-};
--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/bcc536512fb90b51e939cd2b4e9c5958.js
+++
@@ -1,8 +0,0 @@
-import Avatar from "../../../../../src/components/avatar/Avatar";
-import { Playground } from 'docz';
-import * as React from 'react';
-export default {
-  Avatar,
-  Playground,
-  React
-};
