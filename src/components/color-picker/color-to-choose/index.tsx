397b1d79e1edf36a8a2fdaebb56d1cf062b0a4f8
Uma Krishnan <u.krishnan@gmail.com>
2020-10-17 22:15:33 -0700

    radius

--- src/components/color-picker/color-to-choose/index.tsx
+++ src/components/color-picker/color-to-choose/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { withStyles, WithStyles } from '@material-ui/core'
 import { PaletteColor } from 'theme/palette'
-import styles from './styles'
 
-interface IProps extends WithStyles<typeof styles> {
+interface IProps  {
   color: PaletteColor
   handleClick?: (e: any, color: PaletteColor) => void
 }
 
 const ColorToChoose = (props: IProps) => {
-  const { classes, color, handleClick, ...rest } = props
+  const { color, handleClick, ...rest } = props
 
   return (
     <div
-      className={`${classes.root} cursor-pointer inline-block`}
+      className={`w-12 h-12 rounded-full cursor-pointer inline-block`}
       style={{ background: color.bg }}
       onClick={(e: any) => handleClick && color && handleClick(e, color)}
       {...rest}
@@ -21,4 +20,4 @@ const ColorToChoose = (props: IProps) => {
   )
 }
 
-export default withStyles(styles)(ColorToChoose)
+export default ColorToChoose
--- src/components/color-picker/color-to-choose/styles.ts
+++
@@ -1,9 +0,0 @@
-import { createStyles, Theme } from '@material-ui/core'
-
-export default (theme: Theme) =>
-  createStyles({
-    root: {
-      width: '3rem',
-      height: '3rem',
-    },
-  })
